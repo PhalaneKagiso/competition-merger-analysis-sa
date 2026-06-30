294867548fc6b53e51841d4b96acf0de6a833239
PhalaneKagiso <phalanekagiso7@gmail.com>
2026-06-30 06:04:12 +0200

    intitial competition merger analysis

---
+++ scripts/01_project_setup.R
@@ -0,0 +1,9 @@
+# Competition Merger Analysis - South Africa
+# Project: Shoprite & Pick n Pay Merger
+# Author: Phalane Kagiso
+
+
+rm(list=ls())
+library(tidyverse)
+cat("Competition Merger Analysis Project\n")
+cat("Project loaded successfully!\n")
